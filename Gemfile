4723968030068ff64a000f002b347ff7a0cf05d6
Pradeep Sinha <pradeep.sinha@gmail.com>
2011-03-09 15:45:54 +0530

    adding the project skeleton

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'http://rubygems.org'
+gem 'rails', '3.0.5'
+gem 'mysql2'
+
+group :development, :test do
+  gem 'cucumber-rails'
+  gem 'capybara'
+  gem 'database_cleaner'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mindr::Application.config.secret_token = '90dd00a140aebdb31694d1662c2f368a84473e0cd8d018383abd068dfd034f56cbe5e706efc6b91b1e9560b6525051cf05281c1c8beaf61f501aa0af0958c947'
